217967942229b29e81dfd9e114e8a4cbede7e0a1
peng.chen <peng.chen@servingcloud.com>
2019-03-06 14:44:00 +0800

    更新 手动垃圾回收

--- src/main/java/com/github/testerror/fy4a/earth/wallpaper/FY4AEarthWallpaperApp.java
+++ src/main/java/com/github/testerror/fy4a/earth/wallpaper/FY4AEarthWallpaperApp.java
@@ -18,34 +18,38 @@ public class FY4AEarthWallpaperApp {
 	public static final String outPut = System.getProperty("user.dir") + "\\output.jpg";
 
 	public static void main(String [] args) throws IOException, InterruptedException {
-
-
+		run();
 		while(true){
-			Thumbnails.Builder fileBuilder = Thumbnails.fromURLs(Collections.singleton(new URL("http://img.nsmc.org.cn/CLOUDIMAGE/FY4A/MTCC/FY4A_DISK.JPG?random=" + System.currentTimeMillis())));
-			BufferedImage bufferedImage = new BufferedImage(
-					190,
-					80,
-					BufferedImage.TYPE_INT_RGB
-			);
-
-			BufferedImage lopImage = new BufferedImage(
-					60,
-					60,
-					BufferedImage.TYPE_INT_RGB
-			);
-			fileBuilder.watermark(Positions.TOP_LEFT,bufferedImage,0.8f);
-
-			fileBuilder.watermark(Positions.BOTTOM_RIGHT,lopImage,1f);
-
-
-			fileBuilder.scale(0.4).toFile(outPut);
-			SPI.INSTANCE.SystemParametersInfo(0x0014, 0,outPut,0);
-
-			System.out.println("当前图片路径"+outPut);
-			System.out.println("替换成功");
-
+			System.gc();
 			Thread.sleep(1200000L);
+			run();
 		}
+	}
+
+	public static void run() throws IOException {
+
+		Thumbnails.Builder fileBuilder = Thumbnails.fromURLs(Collections.singleton(new URL("http://img.nsmc.org.cn/CLOUDIMAGE/FY4A/MTCC/FY4A_DISK.JPG?random=" + System.currentTimeMillis())));
+		BufferedImage bufferedImage = new BufferedImage(
+				190,
+				80,
+				BufferedImage.TYPE_INT_RGB
+		);
+
+		BufferedImage lopImage = new BufferedImage(
+				60,
+				60,
+				BufferedImage.TYPE_INT_RGB
+		);
+		fileBuilder.watermark(Positions.TOP_LEFT,bufferedImage,0.8f);
+
+		fileBuilder.watermark(Positions.BOTTOM_RIGHT,lopImage,1f);
+
+		fileBuilder.scale(0.4).toFile(outPut);
+		SPI.INSTANCE.SystemParametersInfo(0x0014, 0,outPut,0);
+
+		System.out.println("当前图片路径"+outPut);
+		System.out.println("替换成功");
+
 	}
 
 	public interface SPI extends StdCallLibrary {
